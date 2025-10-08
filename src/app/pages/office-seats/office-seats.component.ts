9b29253da95ae35e0e74a689fef8979e6a89eea7
cosmeenelul <cosmi.gabriel212004@gmail.com>
2025-10-08 12:44:08 +0300

    add selected-seat component

--- src/app/pages/office-seats/office-seats.component.ts
+++ src/app/pages/office-seats/office-seats.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CalendarSeatsComponent } from '../../calendar-seats/calendar-seats.component';
 import { SeatSelectorComponent } from '../../seat-selector/seat-selector.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-office-seats',
   standalone: true,
-  imports: [CalendarSeatsComponent, SeatSelectorComponent],
+  imports: [CalendarSeatsComponent, SeatSelectorComponent, CommonModule],
   templateUrl: './office-seats.component.html',
   styleUrl: './office-seats.component.css',
 })
 export class OfficeSeatsComponent {
   date: Date[] | undefined;
+  selectedFloor: string | null = null;
+  selectedSeat: number | null = null;
+
+  onSeatSelected(event: { floor: string; seat: number }) {
+    this.selectedFloor = event.floor;
+    this.selectedSeat = event.seat;
+  }
 }
--- src/app/seat-selector/seat-selector.component.ts
+++ src/app/seat-selector/seat-selector.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'app-seat-selector',
@@ -9,5 +9,23 @@ import { MatTabsModule } from '@angular/material/tabs';
   styleUrl: './seat-selector.component.css',
 })
 export class SeatSelectorComponent {
-  seats = Array.from({ length: 14 }, (_, i) => i + 1);
+  @Output() seatSelected = new EventEmitter<{ floor: string; seat: number }>();
+
+  seats = Array.from({ length: 17 }, (_, i) => i + 1);
+  selectedSeat: number | null = null;
+  selectedFloor: string = 'Parter';
+
+  onTabChange(event: any) {
+    const labels = ['Parter', 'Etaj 1', 'Etaj 2'];
+    this.selectedFloor = labels[event.index];
+    this.selectedSeat = null;
+  }
+
+  selectSeat(seat: number) {
+    this.selectedSeat = seat;
+    this.seatSelected.emit({
+      floor: this.selectedFloor,
+      seat: seat,
+    });
+  }
 }
