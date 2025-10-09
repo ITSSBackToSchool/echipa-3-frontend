c85d03820362add568266b03348b4586e5e8ba04
cosmeenelul <cosmi.gabriel212004@gmail.com>
2025-10-09 11:39:57 +0300

    finish my-bookings page

--- src/app/pages/my-bookings/my-bookings.component.ts
+++ src/app/pages/my-bookings/my-bookings.component.ts
@@ -5,8 +5,14 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './my-bookings.component.html',
-  styleUrl: './my-bookings.component.css'
+  styleUrl: './my-bookings.component.css',
 })
 export class MyBookingsComponent {
+  selected: string = 'active'; // butonul selectat inițial
 
+  select(status: string) {
+    this.selected = status;
+    console.log('Buton selectat:', status);
+    // aici poți pune logica ta, ex: filtrare carduri
+  }
 }
